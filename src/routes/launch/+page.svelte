32645a63bdc6b6b870c3798901b89a69bbb62933
Milansuman <milansuman2016@gmail.com>
2024-10-20 22:11:23 +0530

    minor stuff

--- src/routes/launch/+page.svelte
+++ src/routes/launch/+page.svelte
@@ -10,10 +10,10 @@
         })
         tl.to(cursor, {
             opacity: 0,
-            duration: 1
+            duration: 0.6
         }).to(cursor, {
             opacity: 1,
-            duration: 1
+            duration: 0.6
         })
     })
 </script>
@@ -21,8 +21,8 @@
 <main class="flex flex-col items-center justify-center h-screen gap-12">
     <img src="/cya.webp" alt="CYA logo"/>
     <div class="w-full aspect-video absolute bg-neutral-950 -z-10 blur-3xl"></div>
-    <div class="p-6 border-2 border-neutral-50 rounded-lg shadow-2xl shadow-green-950 flex flex-row gap-4">
-        <a href="/" class="font-bold text-6xl text-neutral-50">LAUNCH</a>
-        <div class="w-8 h-full bg-white" bind:this={cursor}></div>
+    <div class="p-6 border-2 border-neutral-50 rounded-xl shadow-2xl shadow-green-950 flex flex-row gap-4 items-center">
+        <a href="/" class="font-bold text-7xl text-neutral-50 font-[Jersey10]">LAUNCH</a>
+        <div class="w-8 h-12 bg-white" bind:this={cursor}></div>
     </div>
 </main>
