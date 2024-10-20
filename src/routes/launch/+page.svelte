aa9aa4f126118db67506bc01a1eb53197b2b32d1
Milansuman <milansuman2016@gmail.com>
2024-10-20 22:04:40 +0530

    add launch page

---
+++ src/routes/launch/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    import {gsap} from "gsap";
+    import {onMount} from "svelte"
+
+    let cursor: HTMLDivElement;
+
+    onMount(() => {
+        const tl = gsap.timeline({
+            repeat: -1
+        })
+        tl.to(cursor, {
+            opacity: 0,
+            duration: 1
+        }).to(cursor, {
+            opacity: 1,
+            duration: 1
+        })
+    })
+</script>
+
+<main class="flex flex-col items-center justify-center h-screen gap-12">
+    <img src="/cya.webp" alt="CYA logo"/>
+    <div class="w-full aspect-video absolute bg-neutral-950 -z-10 blur-3xl"></div>
+    <div class="p-6 border-2 border-neutral-50 rounded-lg shadow-2xl shadow-green-950 flex flex-row gap-4">
+        <a href="/" class="font-bold text-6xl text-neutral-50">LAUNCH</a>
+        <div class="w-8 h-full bg-white" bind:this={cursor}></div>
+    </div>
+</main>
